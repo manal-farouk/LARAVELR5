4adc6bad9b7372767096e636c9130eb7f144c2cb
unknown <manalferr@gmail.com>
2024-04-25 06:27:53 +0200

    secont commit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('Manal/{id}' , function($id){
+Route::get('manalferr/{id}' , function($id){
 
     return 'This is my first project at Laravel ' . $id ;
 
