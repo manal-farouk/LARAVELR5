27afea812b8781b614cf2188acfc7fa93da9a525
Manal-Farouk <manalferr@gmail.com>
2024-04-25 23:45:19 +0200

    session 2

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,69 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
+
+Route::get('test50', [MyController::class, 'my_data']);
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('manalferr/{id}' , function($id){
+// Route::get('manalferr/{id?}' , function($id=0){
 
-    return 'This is my first project at Laravel ' . $id ;
+//     return 'This is my first project at Laravel ' . $id ;
 
-}
-);
+// }
+// )
+// ->where(['id'=> '[0-9]+']);
+
+
+// Route::get('manalferr/{id?}' , function($id=0){
+
+//     return 'This is my first project at Laravel ' . $id ;
+
+// }
+// )
+// ->whereNumber(['id']);
+
+// Route::get('girl/{name?}', function ($name) {
+
+//     return 'The girl name is:' . $name;
+
+//     })->whereIn('name',['Manal','Mona','Rana']);
+
+    // Route::prefix('cars')->group(function () {
+
+    //     Route::get('BMW', function(){
+    //     return 'Category is BMW';
+    //     });
+
+    //     Route::get('mercedes', function(){
+    //     return 'Category is Mercedes ';
+    //     });
+    //     });
+
+        // Route::fallback(function() {
+        //    // return 'The requierd is not found';
+        //     return redirect('/');
+        //     });
+            
+
+// Route::get('test', function(){
+// return view('test');
+// });
+
+Route::get('form1', function(){
+    return view('form1');
+    });
+
+    Route::post('recForm1', function() {
+        return 'data received';
+        })->name('receiveForm1');
+
+       
+        
+    
+            
+            
+        
+ 
